a53bcf2ed73e07fe23bbbd6d471d8469f5d2e3c8
chbmb <chbmb@linuxserver.io>
2019-10-13 10:18:32 +0100

    Remove unraidmd
    Already built via Rsync from kernel source

--- build_scripts/variables.sh
+++ build_scripts/variables.sh
@@ -111,7 +111,7 @@ declare -A oot_driver_map=(
         ["6.7.1"]="ixgbe,tehuti"
         ["6.7.2"]="ixgbe,tehuti"
         ["6.7.3-rc4"]="ixgbe,tehuti"
-        ["6.8.0-rc1"]="tehuti,rocketnvme,unraidmd"
+        ["6.8.0-rc1"]="tehuti,rocketnvme"
 )
 
 export OOT_DRIVERS="${oot_driver_map[$UNRAID_DOWNLOAD_VERSION]}"
