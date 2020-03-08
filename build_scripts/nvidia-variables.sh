76a1e577f8ee38f9ab0f8a5c359ed06952c34dde
Daniel Brooks <bass_rock@me.com>
2020-03-08 16:53:54 -0700

    seperating out oot drivers

--- build_scripts/nvidia-variables.sh
+++ build_scripts/nvidia-variables.sh
@@ -60,20 +60,6 @@ xrandr \
 zlib \
 )
 
-
-export STOCK_DIR=${D}/${UNRAID_VERSION}/stock/
-export MODIFIED_DIR=${D}/${UNRAID_VERSION}/nvidia/
-
-mkdir -p ${D}/${UNRAID_VERSION}
-mkdir -p ${STOCK_DIR}
-mkdir -p ${MODIFIED_DIR}
-
-export ROOT_FILES=${D}/unraid-packages/rootfiles
-mkdir -p ${D}/unraid-packages
-mkdir -p ${ROOT_FILES}
-chmod -R 755 ${ROOT_FILES}
-chown -R root:root ${ROOT_FILES}
-
 ##current NVIDIA Release from Slackbuild
 echo -e "${BLUE}Install Packages.sh${NC}    -----    current NVIDIA Release - "
 export NVIDIA_DRIVER_VERSION="$(curl -silent https://slackbuilds.org/slackbuilds/14.2/system/nvidia-driver/nvidia-driver.info | grep VERSION= | cut -d"=" -f2 | sed -e 's/^"//' -e 's/"$//')"
