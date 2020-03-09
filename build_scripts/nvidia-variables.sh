3903a2504435d2a7e7b50daccca6fac7ce50eafb
Daniel Brooks <bass_rock@me.com>
2020-03-08 18:23:25 -0700

    seperating out oot drivers

--- build_scripts/nvidia-variables.sh
+++ build_scripts/nvidia-variables.sh
@@ -2,62 +2,62 @@
 set -e
 
 ##set our package list - Note gcc-go fixed for now until LT upgrade to GCC v9.x
-export nvidia_package_current=(\
-atk \
-bzip2 \
-cairo \
-cairomm \
-dbus \
-expat \
-fontconfig \
-freetype \
-fribidi \
-gcc-go \
-gdk-pixbuf2 \
-glib2 \
-gtk+2 \
-gtk+3 \
-gtkmm2 \
-gtkmm3 \
-graphite2 \
-shared-mime-info \
-harfbuzz \
-libdrm \
-libffi \
-libglvnd \
-libpciaccess \
-libpng \
-libpthread-stubs \
-libvdpau \
-libX11 \
-libXau \
-libxcb \
-libXdamage \
-libXdmcp \
-libXext \
-libXfixes \
-libXrandr \
-libXrender \
-libxshmfence \
-libXv \
-libXvMC \
-libXxf86vm \
-libzip \
-mesa \
-opencl-headers \
-pango \
-pangomm \
-pkgtools \
-pcre2 \
-pcre \
-pixman \
-util-linux \
-xcb-proto \
-xorgproto \
-xorg-server \
-xrandr \
-zlib \
-)
+export nvidia_package_current="
+atk,
+bzip2,
+cairo,
+cairomm,
+dbus,
+expat,
+fontconfig,
+freetype,
+fribidi,
+gcc-go,
+gdk-pixbuf2,
+glib2,
+gtk+2,
+gtk+3,
+gtkmm2,
+gtkmm3,
+graphite2,
+shared-mime-info,
+harfbuzz,
+libdrm,
+libffi,
+libglvnd,
+libpciaccess,
+libpng,
+libpthread-stubs,
+libvdpau,
+libX11,
+libXau,
+libxcb,
+libXdamage,
+libXdmcp,
+libXext,
+libXfixes,
+libXrandr,
+libXrender,
+libxshmfence,
+libXv,
+libXvMC,
+libXxf86vm,
+libzip,
+mesa,
+opencl-headers,
+pango,
+pangomm,
+pkgtools,
+pcre2,
+pcre,
+pixman,
+util-linux,
+xcb-proto,
+xorgproto,
+xorg-server,
+xrandr,
+zlib
+"
 
 ##current NVIDIA Release from Slackbuild
 export NVIDIA_DRIVER_VERSION="$(curl -silent https://slackbuilds.org/slackbuilds/14.2/system/nvidia-driver/nvidia-driver.info | grep VERSION= | cut -d"=" -f2 | sed -e 's/^"//' -e 's/"$//')"
--- build_scripts/variables.sh
+++ build_scripts/variables.sh
@@ -2,44 +2,44 @@
 set -e
 
 ##set our package list - Note gcc & gcc-g++ fixed for now until LT upgrade to GCC v9.x
-export slack_package_current=(\
-autoconf \
-automake \
-bc \
-binutils \
-bison \
-cpio \
-elfutils \
-flex \
-gc \
-gcc \
-gcc-g++ \
-git \
-glibc \
-glibc-solibs \
-guile \
-gettext \
-isl \
-kernel-headers \
-kernel-modules \
-lftp \
-libcgroup \
-libgudev \
-libmpc \
-libtool \
-libunistring \
-libffi \
-libmnl \
-m4 \
-make \
-mpfr \
-ncurses \
-patch \
-perl \
-pkg-config \
-python \
-readline \
-sqlite \
-squashfs-tools \
-zstd \
-)
+export slack_package_current="
+autoconf,
+automake,
+bc,
+binutils,
+bison,
+cpio,
+elfutils,
+flex,
+gc,
+gcc,
+gcc-g++,
+git,
+glibc,
+glibc-solibs,
+guile,
+gettext,
+isl,
+kernel-headers,
+kernel-modules,
+lftp,
+libcgroup,
+libgudev,
+libmpc,
+libtool,
+libunistring,
+libffi,
+libmnl,
+m4,
+make,
+mpfr,
+ncurses,
+patch,
+perl,
+pkg-config,
+python,
+readline,
+sqlite,
+squashfs-tools,
+zstd
+"
